231a5775307ea60aa2fd817266477ab8f3097f31
Rozan MUSTAFA <rozanalmustafa@gmail.com>
2018-08-31 18:06:50 +0300

    Add files via upload

---
+++ go/matrix/todelete.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type Matrix [][]int
+
+// New creates a new matrix from a string
+func main() {
+	m := Matrix{}
+	s := "1 2 3\n4 5 6"
+	
